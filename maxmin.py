ce5288ab2b626a889706a3c21186b9c20a6fb66b
Angelo Beleno <trainee.angelo.beleno@sigmalabs.co.uk>
2023-09-18 22:29:52 +0100

    adding the work

---
+++ maxmin.py
@@ -0,0 +1,13 @@
+
+def max_min(list):
+    min = max = list[0]
+    for number in list:
+        if number > max:
+            max = number
+        if number < min:
+            min = number
+    return [min,max]
+
+# sliced the input to omit the square brackets as input is a literal string of a list
+number_list = input("Please provide a list of integers:")[1:-1].split(", ")
+print(max_min(number_list))
